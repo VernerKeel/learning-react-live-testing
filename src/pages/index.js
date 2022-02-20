5f16d473d5b5062e258f0735add90269bcc15fbd
Verner <verner.keel@gmail.com>
2022-02-20 06:13:42 +0000

    UseEffect inside the component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,20 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 
 // https://bundlephobia.com/ --> a creat tool to find out how much kb a package adds to your bundle
-
 
 //import loadable from "@loadable/component"
 
 //const Data = loadable(() => import("../components/Data"))
 //const Data = setTimeout(() => React.lazy(() => import("../components/Data"))[5000])
-const yo = useEffect(() =>  React.lazy(() => import("../components/Data")))
+
 // Dig deeper on how hooks work perfomance wise:
 //https://hackernoon.com/adventuring-into-react-hooks-performance-practices-rly36xq
 
 // What if I create a separate component that uses useAdvice & useStudy data mega
 // data structure -> can I defer or conditionally not load it by using loadable?
 
-
-
-
 const HomePage = () => {
-
+  const yo = useEffect(() => React.lazy(() => import("../components/Data")))
   return (
     <div>
       <p>Hello world!</p>
@@ -27,4 +23,3 @@ const HomePage = () => {
 }
 
 export default HomePage
-
