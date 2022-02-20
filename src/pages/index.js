770b314a854724b53e44d5e6cc0400faf625a9ac
Verner <verner.keel@gmail.com>
2022-02-20 05:57:14 +0000

    Added set timeout to Data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 
+// https://bundlephobia.com/ --> a creat tool to find out how much kb a package adds to your bundle
+
+
 //import loadable from "@loadable/component"
 
 //const Data = loadable(() => import("../components/Data"))
-const Data = React.lazy(() => import("../components/Data"))
+const Data = setTimeout(() => React.lazy(() => import("../components/Data"))[5000])
 // Dig deeper on how hooks work perfomance wise:
 //https://hackernoon.com/adventuring-into-react-hooks-performance-practices-rly36xq
 
