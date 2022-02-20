50e77fb15abe57c4086381f5aeb16ed9e432e5ec
Verner <verner.keel@gmail.com>
2022-02-20 05:59:18 +0000

    Added useEffect to data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect} from "react"
 
 // https://bundlephobia.com/ --> a creat tool to find out how much kb a package adds to your bundle
 
@@ -6,12 +6,16 @@ import React from "react"
 //import loadable from "@loadable/component"
 
 //const Data = loadable(() => import("../components/Data"))
-const Data = setTimeout(() => React.lazy(() => import("../components/Data"))[5000])
+//const Data = setTimeout(() => React.lazy(() => import("../components/Data"))[5000])
+const yo = useEffect(() => React.lazy(() => import("../components/Data"))[5000]) 
 // Dig deeper on how hooks work perfomance wise:
 //https://hackernoon.com/adventuring-into-react-hooks-performance-practices-rly36xq
 
 // What if I create a separate component that uses useAdvice & useStudy data mega
 // data structure -> can I defer or conditionally not load it by using loadable?
+
+
+
 
 const HomePage = () => {
 
