7891c09347f0438b0a2179479a352357576db855
Verner <verner.keel@gmail.com>
2022-02-24 08:39:13 +0000

    Cofiguring css

--- src/pages/hooks/use-effect.js
+++ src/pages/hooks/use-effect.js
@@ -1,26 +1,30 @@
 import React, { useEffect } from "react"
 import { Link } from "gatsby"
+import { shuffleArr } from "../../utils"
 import { useAdviceData } from "../../hooks"
 import { Img } from "../../components"
-import "./use-effect.css"
+import { list } from "./use-effect.module.css"
 
 const UseEffectPage = () => {
   const [advice] = useAdviceData()
-  console.log(advice)
+  shuffleArr(advice)
   return (
-    <>
+    <div>
       <h1>useEffect()</h1>
       <Link to="/">Back to home</Link>
-      <ul>
-        {advice.map(item => {
-          return (
-            <li>
-              <Img image={item.tag.serviceImage} />
-            </li>
-          )
-        })}
+      <ul className={list}>
+        {advice
+          .filter(item => item.tag.serviceImage !== null)
+          .map(item => {
+            return (
+              <li>
+                <Img image={item.tag.serviceImage} className="img" />
+              </li>
+            )
+          })}
       </ul>
-    </>
+      <button>Not doing anything</button>
+    </div>
   )
 }
 
